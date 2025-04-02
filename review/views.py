2116b3eee1f66cc63aec538e9a03592fd700f339
Raul Campos <raulcampos.dev@gmail.com>
2025-04-02 14:49:57 -0300

    urls and bootstrap frontend basic

--- review/views.py
+++ review/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Review 
 
-# Create your views here.
+def review_list(request):
+    reviews = Review.objects.filter(status='10')
+    return render(request, "review/list.html", {"reviews": reviews})    
+
+def review_detail(request, id):
+    review = Review.objects.get(id=id)
+    return render(request, "review/detail.html", {"review": review})
